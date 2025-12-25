90557b0a3a7b88b4419a02e1a1006459099a80a1
joknarf <joknarf@free.fr>
2025-12-25 20:48:44 +0100

    latest neonfiglet

--- Formula/neonfiglet.rb
+++ Formula/neonfiglet.rb
@@ -1,7 +1,7 @@
 class Neonfiglet < Formula
   desc "Animated Fun Banners"
   homepage "https://github.com/joknarf/neonfiglet"
-  url ""
+  url "https://github.com/joknarf/neonfiglet/releases/download/latest/neonfiglet.tar.gz"
   sha256 "29b0949a0cdf03c03e0a8132edb3a639f2f255878e60b89dd876ff0362feea4b"
   license "MIT"
 
