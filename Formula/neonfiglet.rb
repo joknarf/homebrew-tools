d007a7bc7f7b2d28cc3b5313aa0ba614e0b26ab6
joknarf <joknarf@free.fr>
2025-12-25 14:35:13 +0100

    brew neonfiglet

---
+++ Formula/neonfiglet.rb
@@ -0,0 +1,16 @@
+class Neonfiglet < Formula
+  desc "Animated Fun Banners"
+  homepage "https://github.com/joknarf/neonfiglet"
+  url "https://github.com/joknarf/neonfiglet/releases/download/1.2.13/neonfiglet.tar.gz"
+  sha256 "058812e8e6fc8b16a4e244ee0af888efdb3bc857c402c7194c807d8be0183ef7"
+  license "MIT"
+
+  def install
+    bin.install "neonfiglet"
+  end
+
+  test do
+    system "#{bin}/neonfiglet", "neonfiglet"
+  end
+end
+
