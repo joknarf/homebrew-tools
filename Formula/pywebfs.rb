21c85f745b8d838b39b83efbdf66a9fe07dea902
joknarf <joknarf@free.fr>
2025-12-30 14:54:32 +0100

    pywebfs

---
+++ Formula/pywebfs.rb
@@ -0,0 +1,27 @@
+class Portune < Formula
+  desc "simple HTTP file server"
+  homepage "https://github.com/joknarf/portune"
+  version "1.0.14"
+  license "MIT"
+
+  AMD64 = { url: "https://github.com/joknarf/portune/releases/download/1.0.14/portune-darwin-amd64.tar.gz", sha256: "" }
+  ARM64 = { url: "https://github.com/joknarf/portune/releases/download/1.0.14/portune-darwin-arm64.tar.gz", sha256: "" }
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url AMD64[:url]
+      sha256 AMD64[:sha256]
+    end
+
+    if Hardware::CPU.arm?
+      url ARM64[:url]
+      sha256 ARM64[:sha256]
+    end
+  end
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"pywebfs"
+  end
+
+end
